8989e25c2c973381fd0f99af20018698c52277bb
KtonSilva <kleytonlsilva@gmail.com>
2024-06-30 00:50:02 -0300

    W2 commit

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,4 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
     const currentYear = new Date().getFullYear();
     document.getElementById("currentyear").textContent = currentYear;
     document.getElementById("lastModified").textContent = "Last Updated: " + document.lastModified;
+
+    const hamburger = document.getElementById("hamburger");
+    const nav = document.querySelector("nav");
+
+    hamburger.addEventListener("click", function() {
+        nav.classList.toggle("active");
+    });
 });
